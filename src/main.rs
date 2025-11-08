353ed11698ec887ed654b6304e4aff82345a338e
simbullar <simbullar@gmail.com>
2025-11-08 22:06:28 +0100

    added the multiple instance detection

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
 use dispatch::Queue;
 use fd_lock::*;
-use std::fs::*;
+use objc2_app_kit::NSTextStorageEditActions;
+use std::io::Write;
 use std::os::unix::net::UnixListener;
 use std::path::Path;
+use std::{fs::*, os::unix::net::UnixStream};
 
 mod search;
 mod window;
 
 use objc2_foundation::{
-    NSFileManager, NSSearchPathDirectory, NSSearchPathDomainMask,
-    NSSearchPathForDirectoriesInDomains,
+    NSSearchPathDirectory, NSSearchPathDomainMask, NSSearchPathForDirectoriesInDomains,
 };
 
 fn app_support_path() -> String {
-    unsafe {
-        let paths = NSSearchPathForDirectoriesInDomains(
-            NSSearchPathDirectory::ApplicationSupportDirectory,
-            NSSearchPathDomainMask::UserDomainMask,
-            true,
-        );
-        let path = paths.firstObject().unwrap();
-        let path_str = path.to_string();
-        let bundlename = "runner";
-        format!("{}/{}/", path_str, bundlename)
-    }
+    let paths = NSSearchPathForDirectoriesInDomains(
+        NSSearchPathDirectory::ApplicationSupportDirectory,
+        NSSearchPathDomainMask::UserDomainMask,
+        true,
+    );
+    let path = paths.firstObject().unwrap();
+    let path_str = path.to_string();
+    let bundlename = "runner";
+    format!("{}/{}", path_str, bundlename)
 }
-fn check_for_appsupport_dir(path: String) -> String {
+fn check_for_appsupport_dir(path: &String) -> String {
     let file_path = format!("{}/lockfile.lock", path);
     if !Path::new(path.as_str()).exists() {
         let _ = create_dir(&path); // Create the application directory if it doesn't exist
@@ -35,7 +34,8 @@ fn check_for_appsupport_dir(path: String) -> String {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app_path = app_support_path();
-    let lock_path = check_for_appsupport_dir(app_path);
+    let lock_path = check_for_appsupport_dir(&app_path);
+    let socket_path = format!("{}/ipc.sock", &app_path);
 
     let file = OpenOptions::new()
         .read(true)
@@ -44,33 +44,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .open(&lock_path)?;
     let mut f = RwLock::new(file);
 
-    let guard = match f.try_write() {
+    let _guard = match f.try_write() {
         Ok(guard) => {
             println!("First process...");
             guard
         }
         Err(_) => {
             println!("Second process...");
+            let mut stream = UnixStream::connect(&socket_path)?;
+            //stream.write(b"");
             std::process::exit(0);
         }
     };
-
-    /* let main_queue = Queue::main();
-    std::thread::spawn(move || {
-        loop {
-            let should_toggle = check_ipc_or_search();
-
-            main_queue.exec_async(move || {
-                toggle_window_visibility(); // Must run on main thread
-            });
-        }
-    }); */
 
     let mtm = objc2_foundation::MainThreadMarker::new().unwrap();
 
     let app = objc2_app_kit::NSApplication::sharedApplication(mtm);
     let delegate = window::Delegate::new(mtm);
     app.setDelegate(Some(objc2::runtime::ProtocolObject::from_ref(&*delegate)));
+
+    let main_queue = Queue::main();
+    let _ = remove_file(&socket_path);
+    let listener = UnixListener::bind(&socket_path)?;
+    std::thread::spawn(move || {
+        loop {
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(stream) => {
+                        main_queue
+                            .exec_async({ move || println!("A wild second instance appears!") });
+                    }
+                    Err(err) => {
+                        break;
+                    }
+                }
+            }
+        }
+    });
 
     app.run();
 
