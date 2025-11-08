7f07b15e9aecc25567368a0351ac015736ec19a3
simbullar <simbullar@gmail.com>
2025-11-08 20:58:32 +0100

    added multiple-process detection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,71 @@
+use dispatch::Queue;
+use fd_lock::*;
+use std::fs::*;
+use std::os::unix::net::UnixListener;
+use std::path::Path;
+
 mod search;
 mod window;
 
-fn main() {
+use objc2_foundation::{
+    NSFileManager, NSSearchPathDirectory, NSSearchPathDomainMask,
+    NSSearchPathForDirectoriesInDomains,
+};
+
+fn app_support_path() -> String {
+    unsafe {
+        let paths = NSSearchPathForDirectoriesInDomains(
+            NSSearchPathDirectory::ApplicationSupportDirectory,
+            NSSearchPathDomainMask::UserDomainMask,
+            true,
+        );
+        let path = paths.firstObject().unwrap();
+        let path_str = path.to_string();
+        let bundlename = "runner";
+        format!("{}/{}/", path_str, bundlename)
+    }
+}
+fn check_for_appsupport_dir(path: String) -> String {
+    let file_path = format!("{}/lockfile.lock", path);
+    if !Path::new(path.as_str()).exists() {
+        let _ = create_dir(&path); // Create the application directory if it doesn't exist
+    }
+    file_path
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app_path = app_support_path();
+    let lock_path = check_for_appsupport_dir(app_path);
+
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&lock_path)?;
+    let mut f = RwLock::new(file);
+
+    let guard = match f.try_write() {
+        Ok(guard) => {
+            println!("First process...");
+            guard
+        }
+        Err(_) => {
+            println!("Second process...");
+            std::process::exit(0);
+        }
+    };
+
+    /* let main_queue = Queue::main();
+    std::thread::spawn(move || {
+        loop {
+            let should_toggle = check_ipc_or_search();
+
+            main_queue.exec_async(move || {
+                toggle_window_visibility(); // Must run on main thread
+            });
+        }
+    }); */
+
     let mtm = objc2_foundation::MainThreadMarker::new().unwrap();
 
     let app = objc2_app_kit::NSApplication::sharedApplication(mtm);
@@ -9,4 +73,6 @@ fn main() {
     app.setDelegate(Some(objc2::runtime::ProtocolObject::from_ref(&*delegate)));
 
     app.run();
+
+    Ok(())
 }
